fcb8f9ee43d5a78616175309da65673ce33faf6d
Jon Rhea <indianarhea@gmail.com>
2021-04-12 23:33:11 -0400

    Added Bomb and Elixir Functionality
    - Added Bomb - Does a set 20 damage to an enemy
    - Added Elixir - Restores 10 MP to a player character

---
+++ GameDesignFinalProject2D/Assets/Scripts/onBomb.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class onBomb : MonoBehaviour
+{
+    public void onBombButton1(){
+		EnemyScript.setHP(EnemyScript.getCurrentHP() - 20);
+		BattleTurn.currentTurn++;
+	}
+	
+	public void onBombButton2(){
+		Enemy2Script.setHP(Enemy2Script.getCurrentHP() - 20);
+		BattleTurn.currentTurn++;
+	}
+}
---
+++ GameDesignFinalProject2D/Assets/Scripts/onElixir.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class onElixir : MonoBehaviour
+{
+    public void onElixirWarrior(){
+		
+		if(WarriorScript.getCurrentMP() + 10 >= WarriorScript.getMaxMP()){
+			WarriorScript.setMP(WarriorScript.getMaxMP());
+		}//end if
+		else{
+			WarriorScript.setMP(WarriorScript.getCurrentMP() + 10);
+		}//end else		
+		BattleTurn.currentTurn++;
+	}//end onElixerWarrior
+	
+	public void onElixirMage(){
+		
+		if(MageScript.getCurrentMP() + 10 >= MageScript.getMaxMP()){
+			MageScript.setHP(MageScript.getMaxMP());
+		}//end if
+		else{
+			MageScript.setMP(MageScript.getCurrentMP() + 10);
+		}//end else
+		
+		BattleTurn.currentTurn++;
+	}//end onElixerMage
+}
