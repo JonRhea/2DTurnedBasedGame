7a463a33da4656dfa6d6aeceeca4bbf88a4a15f8
Jon Rhea <indianarhea@gmail.com>
2021-04-28 14:07:52 -0400

    Replaced Error Sound and Added Error Checks to Attack
    - Replaced Error Sound - The sound is the exact same, but the sound file is now shortened so the sound plays immediately.
    - Added Error Check to Attack - Player characters can no longer attack dead enemies.

--- GameDesignFinalProject2D/Assets/Scripts/onAttack.cs
+++ GameDesignFinalProject2D/Assets/Scripts/onAttack.cs
@@ -7,9 +7,7 @@ public class onAttack : MonoBehaviour
 {
 	
 	Text enemySelectText;
-	//public GameObject playerObject;
-	//Vector3 playerEnd;
-	//bool isMoving = false;
+	public AudioSource errorSound;
 	
 	void Start()
     {
@@ -22,76 +20,55 @@ public class onAttack : MonoBehaviour
 
 
 	public void onAttackButton(){
-		int enemyHPcurrent = EnemyScript.getCurrentHP();
-		//StartCoroutine(moveToX(playerObject.transform, new Vector3(4f, 2.34f, -3.646777f), 1000f));
-		/*playerStart = playerObject.transform.position;
-		temp = playerObject.transform.position;
-		playerEnd = new Vector3(4f, 2.34f, -3.646777f);
 		
-		while(playerObject.transform.position.x != 4f){
+		if(EnemyScript.getCurrentHP() > 0){
+			int enemyHPcurrent = EnemyScript.getCurrentHP();
+		
 			
-			//temp.x += moveSpeed * Time.deltaTime;
-			//playerObject.transform.position = temp;
-			//playerObject.transform.position = playerEnd;
-			print(playerObject.transform.position.x);
-		}//end while
-
-		playerObject.transform.position = playerStart;
-		*/
+			//prints are for debugging
+			print(enemyHPcurrent);
+			print(BattleTurn.playerAttackStat);
+			print(EnemyScript.getDefenseStat());
+			
 		
-		//prints are for debugging
-		print(enemyHPcurrent);
-		print(BattleTurn.playerAttackStat);
-		print(EnemyScript.getDefenseStat());
-		
-	
-		EnemyScript.setHP(enemyHPcurrent - BattleTurn.playerAttackStat + EnemyScript.getDefenseStat() - 10);
-		
-		enemyHPcurrent = EnemyScript.getCurrentHP();
-		print(enemyHPcurrent);
-		BattleTurn.currentTurn++;
-		
-		enemySelectText.gameObject.SetActive(false);
+			EnemyScript.setHP(enemyHPcurrent - BattleTurn.playerAttackStat + EnemyScript.getDefenseStat() - 10);
+			
+			enemyHPcurrent = EnemyScript.getCurrentHP();
+			print(enemyHPcurrent);
+			BattleTurn.currentTurn++;
+			
+			enemySelectText.gameObject.SetActive(false);
+		}//end if
+		else if(EnemyScript.getCurrentHP() <= 0){
+			errorSound.Play();
+			print("attack button 1");
+		}//end if
 	}//end onAttackButton
 	
 	public void onAttackButton2(){
-		int enemy2HPcurrent = Enemy2Script.getCurrentHP();
 		
-		//prints are for debugging
-		print(enemy2HPcurrent);
-		print(BattleTurn.playerAttackStat);
-		print(Enemy2Script.getDefenseStat());
+		if(Enemy2Script.getCurrentHP() > 0){
+			int enemy2HPcurrent = Enemy2Script.getCurrentHP();
+			
+			//prints are for debugging
+			print(enemy2HPcurrent);
+			print(BattleTurn.playerAttackStat);
+			print(Enemy2Script.getDefenseStat());
+			
 		
-	
-		Enemy2Script.setHP(enemy2HPcurrent - BattleTurn.playerAttackStat + Enemy2Script.getDefenseStat() - 10);
-		
-		enemy2HPcurrent = Enemy2Script.getCurrentHP();
-		print(enemy2HPcurrent);
-		BattleTurn.currentTurn++;
-		
-		enemySelectText.gameObject.SetActive(false);
+			Enemy2Script.setHP(enemy2HPcurrent - BattleTurn.playerAttackStat + Enemy2Script.getDefenseStat() - 10);
+			
+			enemy2HPcurrent = Enemy2Script.getCurrentHP();
+			print(enemy2HPcurrent);
+			BattleTurn.currentTurn++;
+			
+			enemySelectText.gameObject.SetActive(false);
+		}//end if
+		else if(Enemy2Script.getCurrentHP() <= 0){
+			errorSound.Play();
+			print("attack button 2");
+		}//end if
 	}//end onAttackButton
 	
-	/*IEnumerator moveToX(Transform fromPosition, Vector3 toPosition, float duration){
-			
-		if (isMoving){
-			yield break;
-		}
-		isMoving = true;
-
-		float counter = 0;
-
-		Vector3 startPos = fromPosition.position;
-
-		while (counter < duration){
-			counter += Time.deltaTime;
-			fromPosition.position = Vector3.Lerp(startPos, toPosition, counter / duration);
-			//yield return null;
-		}
-
-		isMoving = false;
-
-		
-	}//end moveToX
-	*/
+	
 }
